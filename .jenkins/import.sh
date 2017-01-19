c3477e22ab4b11fed37d0aabb5bb55009229a034
Kelvin Paska <k.paska@amsterdam.nl>
2017-01-19 10:44:41 +0100

    TG-554 - changed project name in the import

--- .jenkins/import.sh
+++ .jenkins/import.sh
@@ -6,7 +6,7 @@ set -u
 DIR="$(dirname $0)"
 
 dc() {
-	docker-compose -p nap -f ${DIR}/docker-compose.yml $*
+	docker-compose -p tellus -f ${DIR}/docker-compose.yml $*
 }
 
 trap 'dc kill ; dc rm -f' EXIT
