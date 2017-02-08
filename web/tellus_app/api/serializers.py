1c8df40dc9d9e031ef1888fb5a1a4d2cd7bc79c0
Thijs Creemers <thijs.creemers@gmail.com>
2017-02-08 11:05:06 +0100

    update tests and serialisers to reflect model change

--- web/tellus_app/api/serializers.py
+++ web/tellus_app/api/serializers.py
@@ -70,7 +70,6 @@ class SnelheidsCategorieSerializer(TellusMixin, HALSerializer):
 
 
 class TellusDataSerializer(TellusMixin, HALSerializer):
-    meet_resultaten = serializers.SerializerMethodField()
     _display = serializers.SerializerMethodField()
 
     class Meta:
@@ -86,16 +85,19 @@ class TellusDataSerializer(TellusMixin, HALSerializer):
             'validatie',
             'representatief',
             'meetraai',
-            'meet_resultaten',
+            'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9',
+            'c10', 'c11', 'c12', 'c13', 'c14', 'c15', 'c16', 'c17', 'c18', 'c19',
+            'c20', 'c21', 'c22', 'c23', 'c24', 'c25', 'c26', 'c27', 'c28', 'c29',
+            'c30', 'c31', 'c32', 'c33', 'c34', 'c35', 'c36', 'c37', 'c38', 'c39',
+            'c40', 'c41', 'c42', 'c43', 'c44', 'c45', 'c46', 'c47', 'c48', 'c49',
+            'c50', 'c51', 'c52', 'c53', 'c54', 'c55', 'c56', 'c57', 'c58', 'c59',
+            'c60',
         )
         extra_kwargs = {
             'tellus': {'view_name': 'tellus-detail', 'lookup_field': 'pk'},
             'snelheids_categorie': {'view_name': 'snelheidscategorie-detail', 'lookup_field': 'pk'},
             'lengte_categorie': {'view_name': 'lengtecategorie-detail', 'lookup_field': 'pk'}
         }
-
-    def get_meet_resultaten(self, obj):
-        return json.loads(obj.data)
 
     def get__display(self, obj):
         return str(obj)
