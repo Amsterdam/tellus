7b79243d301ad731eb22dbfd2cb5147feb5fd8ec
Thijs Creemers <thijs.creemers@gmail.com>
2017-03-22 11:17:33 +0100

    replace datapunt url and removed sites

--- web/tellus_app/datasets/tellus_data/migrations/0007_geoview_tellus.py
+++ web/tellus_app/datasets/tellus_data/migrations/0007_geoview_tellus.py
@@ -1,22 +1,6 @@
 from django.db import migrations
-from django.contrib.sites.models import Site
-
-from tellus import settings
 
 API_DOMAIN = 'API Domain'
-
-
-def create_site(apps, *args, **kwargs):
-    Site.objects.filter(name='API Domain').delete()
-
-    Site.objects.update_or_create(
-        domain=settings.DATAPUNT_API_URL,
-        name=API_DOMAIN
-    )
-
-
-def delete_site(apps, *args, **kwargs):
-    Site.objects.filter(name='API Domain').delete()
 
 
 class Migration(migrations.Migration):
@@ -24,22 +8,4 @@ class Migration(migrations.Migration):
         ('tellus_data', '0006_make_pivot_view_dynamic'),
     ]
 
-    operations = [
-
-        migrations.RunPython(code=create_site, reverse_code=delete_site),
-
-        migrations.RunSQL(
-            f"""
-                CREATE VIEW geo_tellus_point AS
-                SELECT
-                  tellus.objnr_leverancier as display,
-                  cast('tellussen/tellus' as varchar(30)) as type,
-                  tellus.standplaats,
-                  site.domain || 'tellus/tellus/' || tellus.id || '/' AS uri,
-                  tellus.geometrie AS geometrie
-                FROM
-                  tellus_data_tellus tellus , django_site site
-                WHERE
-                  tellus.geometrie IS NOT NULL and site.name = '{API_DOMAIN}';
-            """)
-    ]
+    operations = []
--- web/tellus_app/datasets/tellus_data/migrations/0008_domain_api.py
+++ web/tellus_app/datasets/tellus_data/migrations/0008_domain_api.py
@@ -1,22 +1,6 @@
 from django.db import migrations
-from django.contrib.sites.models import Site
-
-from tellus import settings
 
 API_DOMAIN = 'API Domain'
-
-
-def create_site(apps, *args, **kwargs):
-    Site.objects.filter(name='API Domain').delete()
-
-    Site.objects.update_or_create(
-        domain=settings.DATAPUNT_API_URL,
-        name=API_DOMAIN
-    )
-
-
-def delete_site(apps, *args, **kwargs):
-    Site.objects.filter(name='API Domain').delete()
 
 
 class Migration(migrations.Migration):
@@ -24,8 +8,4 @@ class Migration(migrations.Migration):
         ('tellus_data', '0007_geoview_tellus'),
     ]
 
-    operations = [
-
-        migrations.RunPython(code=create_site, reverse_code=delete_site),
-
-    ]
+    operations = []
