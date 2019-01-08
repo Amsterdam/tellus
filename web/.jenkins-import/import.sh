f677a7b5e75d8e073b02f7d4807cd5307afe8bdd
RM <RRMoelker@users.noreply.github.com>
2019-01-08 16:55:27 +0100

    backups dir created right before db dump

--- web/.jenkins-import/import.sh
+++ web/.jenkins-import/import.sh
@@ -12,10 +12,10 @@ dc() {
 trap 'dc kill ; dc rm -f' EXIT
 
 rm -rf ${DIR}/backups
-mkdir -p ${DIR}/backups
 
 dc build
 dc run --rm importer
 
+mkdir -p ${DIR}/backups
 dc exec -T database backup-db.sh tellus
 dc down -v
