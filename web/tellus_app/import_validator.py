015eb3db0b97df1a5406b9b6e6307232994693eb
RM <RRMoelker@users.noreply.github.com>
2019-01-15 12:45:44 +0100

    OP-466, Refresh materialized views as part of import process

--- web/tellus_app/import_validator.py
+++ web/tellus_app/import_validator.py
@@ -44,22 +44,21 @@ def check_import():
     # assert_count(50 * 1000 * 1000, Telling.objects.count(), 'Tellingen count')
     assert_count(40 * 1000 * 1000, Telling.objects.count(), 'Tellingen count')
 
-
     log_leaf_migrations()
 
     log.info("Checking materialized views: ")
     # assert_count(720 * 1000, TellingCarsPerHourPerDay.objects.count(), 'TellingCarsPerHourPerDay count')
-    assert_count(0, TellingCarsPerHourPerDay.objects.count(), 'TellingCarsPerHourPerDay count')
+    assert_count(1000, TellingCarsPerHourPerDay.objects.count(), 'TellingCarsPerHourPerDay count')
     # assert_count(4 * 1000 * 1000, TellingCarsPerHourLength.objects.count(), 'TellingCarsPerHourLength count')
-    assert_count(0, TellingCarsPerHourLength.objects.count(), 'TellingCarsPerHourLength count')
+    assert_count(1000, TellingCarsPerHourLength.objects.count(), 'TellingCarsPerHourLength count')
     # assert_count(7 * 1000 * 1000, TellingCarsPerHourSpeed.objects.count(), 'TellingCarsPerHourSpeed count')
-    assert_count(0, TellingCarsPerHourSpeed.objects.count(), 'TellingCarsPerHourSpeed count')
+    assert_count(1000, TellingCarsPerHourSpeed.objects.count(), 'TellingCarsPerHourSpeed count')
     # assert_count(45 * 1000, TellingCarsYMH.objects.count(), 'TellingCarsYMH count')
-    assert_count(0, TellingCarsYMH.objects.count(), 'TellingCarsYMH count')
+    assert_count(1000, TellingCarsYMH.objects.count(), 'TellingCarsYMH count')
     # assert_count(250 * 1000, TellingCarsYMHLength.objects.count(), 'TellingCarsYMHLength count')
-    assert_count(0, TellingCarsYMHLength.objects.count(), 'TellingCarsYMHLength count')
+    assert_count(1000, TellingCarsYMHLength.objects.count(), 'TellingCarsYMHLength count')
     # assert_count(450 * 1000, TellingCarsYMHSpeed.objects.count(), 'TellingCarsYMHSpeed count')
-    assert_count(0, TellingCarsYMHSpeed.objects.count(), 'TellingCarsYMHSpeed count')
+    assert_count(1000, TellingCarsYMHSpeed.objects.count(), 'TellingCarsYMHSpeed count')
     log.info("Materialized views checked")
 
     log.info('Check done')
