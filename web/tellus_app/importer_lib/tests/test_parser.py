b4a94fb8c45b78c9b33802995d0e3c22b4d385db
RM <RRMoelker@users.noreply.github.com>
2019-03-11 16:20:40 +0100

    expected and actual argument now in right order for parser test

--- web/tellus_app/importer_lib/tests/test_parser.py
+++ web/tellus_app/importer_lib/tests/test_parser.py
@@ -4,27 +4,27 @@ from importer_lib.parser import parse_speed_interval, parse_length_interval
 
 class TestParser(TestCase):
     def test_speed_max_only(self):
-        self.assertEqual(parse_speed_interval("< 30 km/u"), [None, 30])
-        self.assertEqual(parse_speed_interval("< 50 km/u"), [None, 50])
+        self.assertEqual([None, 30], parse_speed_interval("< 30 km/u"))
+        self.assertEqual([None, 50], parse_speed_interval("< 50 km/u"))
 
     def test_speed_min_max(self):
-        self.assertEqual(parse_speed_interval("31 - 40 km/u"), [31, 40])
-        self.assertEqual(parse_speed_interval("91 - 100 km/u"), [91, 100])
+        self.assertEqual([31, 40], parse_speed_interval("31 - 40 km/u"))
+        self.assertEqual([91, 100], parse_speed_interval("91 - 100 km/u"))
 
     def test_speed_min_only(self):
-        self.assertEqual(parse_speed_interval("> 0 km/u"), [0, None])
-        self.assertEqual(parse_speed_interval("> 100 km/u"), [100, None])
-        self.assertEqual(parse_speed_interval("> 120 km/u"), [120, None])
+        self.assertEqual([0, None], parse_speed_interval("> 0 km/u"))
+        self.assertEqual([100, None], parse_speed_interval("> 100 km/u"))
+        self.assertEqual([120, None], parse_speed_interval("> 120 km/u"))
 
     def test_length_max_only(self):
-        self.assertEqual(parse_length_interval("< 5.0 m"), [None, 500])
-        self.assertEqual(parse_length_interval("< 5 m"), [None, 500])
+        self.assertEqual([None, 500], parse_length_interval("< 5.0 m"))
+        self.assertEqual([None, 500], parse_length_interval("< 5 m"))
 
     def test_length_min_max(self):
-        self.assertEqual(parse_length_interval("0 - 5,1 m"), [0, 510])
-        self.assertEqual(parse_length_interval("5,1 - 5,6 m"), [510, 560])
-        self.assertEqual(parse_length_interval("5 - 5 m"), [500, 500])
+        self.assertEqual([0, 510], parse_length_interval("0 - 5,1 m"))
+        self.assertEqual([510, 560], parse_length_interval("5,1 - 5,6 m"))
+        self.assertEqual([500, 500], parse_length_interval("5 - 5 m"))
 
     def test_length_min_only(self):
-        self.assertEqual(parse_length_interval("> 12,2 m"), [1220, None])
-        self.assertEqual(parse_length_interval("> 12 m"), [1200, None])
+        self.assertEqual([1220, None], parse_length_interval("> 12,2 m"))
+        self.assertEqual([1200, None], parse_length_interval("> 12 m"))
