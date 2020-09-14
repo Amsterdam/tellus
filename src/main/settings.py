a712051002facd520321ea744ed12cede1e9a145
Jeffrey Klardie <j.klardie@amsterdam.nl>
2020-09-14 14:15:47 +0200

    Removed unnecessary complex database settings

--- src/main/settings.py
+++ src/main/settings.py
@@ -10,63 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 import os
-import re
 import sys
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-
-def get_docker_host():
-    """
-    Looks for the DOCKER_HOST environment variable to find the VM
-    running docker-machine.
-
-    If the environment variable is not found, it is assumed that
-    you're running docker on localhost.
-    """
-    d_host = os.getenv("DOCKER_HOST", None)
-    if d_host:
-        if re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", d_host):
-            return d_host
-
-        return re.match(r"tcp://(.*?):\d+", d_host).group(1)
-    return "localhost"
-
-
-def in_docker():
-    """
-    Checks pid 1 cgroup settings to check with reasonable certainty we're in a
-    docker env.
-    :return: true when running in a docker container, false otherwise
-    """
-    try:
-        cgroup = open("/proc/1/cgroup", "r").read()
-        return ":/docker/" in cgroup or ":/docker-ce/" in cgroup
-    except:  # noqa
-        return False
-
-
-OVERRIDE_HOST_ENV_VAR = "DATABASE_HOST_OVERRIDE"
-OVERRIDE_PORT_ENV_VAR = "DATABASE_PORT_OVERRIDE"
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-class Location_key:
-    local = "local"
-    docker = "docker"
-    override = "override"
-
-
-def get_database_key():
-    if os.getenv(OVERRIDE_HOST_ENV_VAR):
-        return Location_key.override
-    elif in_docker():
-        return Location_key.docker
-
-    return Location_key.local
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -153,34 +103,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "main.wsgi.application"
 
-DATABASE_OPTIONS = {
-    Location_key.docker: {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": os.getenv("DATABASE_NAME", "dev"),
-        "USER": os.getenv("DATABASE_USER", "dev"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD", "dev"),
-        "HOST": "database",
-        "PORT": "5432",
-    },
-    Location_key.local: {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": os.getenv("DATABASE_NAME", "dev"),
-        "USER": os.getenv("DATABASE_USER", "dev"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD", "dev"),
-        "HOST": get_docker_host(),
-        "PORT": "5409",
-    },
-    Location_key.override: {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": os.getenv("DATABASE_NAME", "dev"),
-        "USER": os.getenv("DATABASE_USER", "dev"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD", "dev"),
-        "HOST": os.getenv(OVERRIDE_HOST_ENV_VAR),
-        "PORT": os.getenv(OVERRIDE_PORT_ENV_VAR, "5432"),
-    },
-}
 
-DATABASES = {"default": DATABASE_OPTIONS[get_database_key()]}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": os.getenv("DATABASE_NAME", "dev"),
+        "USER": os.getenv("DATABASE_USER", "dev"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "dev"),
+        "HOST": os.getenv("DATABASE_HOST", "database"),
+        "PORT": os.getenv("DATABASE_PORT", "5432"),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
