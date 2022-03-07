f3adf4da9ce3042c4d9957f6125294b7f5201ce6
Niels Lensink <nielslensink@gmail.com>
2022-03-07 15:36:36 +0100

    compare lower string comparison for debug

--- src/main/settings.py
+++ src/main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = os.getenv("DEBUG", False) == "True"
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = ["*"]
 
